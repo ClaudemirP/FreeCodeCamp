a3f1e6d226094c36d1ed18ce94d7be88d085b866
Claudemir Ribeiro da Silva <claudemirp@outlook.com>
2022-01-08 06:31:05 -0300

    Update slice-and-splice.js

--- slice-and-splice.js
+++ slice-and-splice.js
@@ -1,3 +1,18 @@
+function frankensplice(arr1, arr2, n){
+    let localArray = arr2.slice()
+    //um loop é criado para passar por todos os indices do arr1
+    for (let i = 0; i < arr1.length; i++){
+        //cada item é incluido separadamente
+        localArray.splice(n, 0, arr1[i])
+        //n fica maior a cada passada do loop para colocar os valores no lugar correto
+        n++
+    }
+    return localArray
+}
+
+console.log(frankensplice([1,2,3], [4,5,6], 2))
+
+
 function frankenstein(arr1, arr2, n){
     //copia o arr2 para a variavel localArray com slice para manter o original intacto
     let localArray = arr2.slice()
@@ -8,3 +23,4 @@ function frankenstein(arr1, arr2, n){
 }
 
 console.log(frankenstein([1,2,3], [4,5,6], 2))
+
