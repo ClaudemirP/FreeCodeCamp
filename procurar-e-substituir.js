211bf5034d2dfd53285aa0e9d33d6ed6f8d940a3
Claudemir Ribeiro da Silva <claudemirp@outlook.com>
2022-01-15 22:32:03 -0300

    Cash Registrer

--- procurar-e-substituir.js
+++ procurar-e-substituir.js
@@ -1,13 +1,10 @@
 function myReplace(str, before, after) {
   //testa se o before começa com letra maiuscula ou minuscula usando o metodo test do regex
-  if (/^[A-Z]/.test(before)){
-    after = after[0].toUpperCase() + after.slice(1)
-  } else {
-    after = after[0].toLowerCase() + after.slice(1)
-  }
+  /^[A-Z]/.test(before) ? after = after[0].toUpperCase() + after.slice(1) : after.toLowerCase()
   //troca eles com replace
   let res = str.replace(before, after)
   return res;
 }
 
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped")
+console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"))
