17adb1f0fe5a061b2f0ab17b652003773e8281e1
Claudemir Ribeiro da Silva <claudemirp@outlook.com>
2022-01-16 08:40:17 -0300

    Update procurar-e-substituir.js

--- procurar-e-substituir.js
+++ procurar-e-substituir.js
@@ -2,8 +2,7 @@ function myReplace(str, before, after) {
   //testa se o before começa com letra maiuscula ou minuscula usando o metodo test do regex
   /^[A-Z]/.test(before) ? after = after[0].toUpperCase() + after.slice(1) : after.toLowerCase()
   //troca eles com replace
-  let res = str.replace(before, after)
-  return res;
+  return str.replace(before, after)
 }
 
 myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped")
